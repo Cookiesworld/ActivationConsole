b6be7457308796aa0eb10fa4c4662f7f099cc217
John Cooke <jdcooke@hotmail.com>
2021-12-01 18:39:32 +0000

    Initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ActivationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Enter the product key and ");
+            }
+
+            string InstallationId = args[0];
+            string ExtendedProductId = args[1];
+
+            var result = ActivationHelper.CallWebService(1, InstallationId, ExtendedProductId);
+            Console.WriteLine(result);
+        }
+    }
+}
